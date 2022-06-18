b9ef6018bb6181a916990f42beb281d718be5525
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-19 02:45:23 +0700

    На карточку товара добавлен вывод текста из верхней части макета с учётом значений передаваемых карточке через props + исправлен баг центрирования контента в ie 11

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
     <div className="content">
       <div className="header">Ты сегодня покормил кота?</div>
       <div className="shopwindow">
-        <Card id={'1'} className="card" />
-        <Card id={'2'} className="card" />
-        <Card id={'3'} className="card" />
+        <Card id={'1'} className="card" taste={'с фуа-гра'} countOfServings={1} countOfMouse={1} />
+        <Card id={'2'} className="card" taste={'с рыбой'} countOfServings={2} countOfMouse={2} />
+        <Card id={'3'} className="card" taste={'с курой'} countOfServings={5} countOfMouse={5} />
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,46 @@ import Underline from '../assets/Underline';
 import Cat from '../assets/cat.png';
 import './Card.css';
 
+function TextCard4({ countOfServings, countOfMouse }) {
+  const text = { text1: '', text2: '', text3: '' };
+
+  if (countOfServings === 1) {
+    text.text1 = 'порция';
+  } else if (countOfServings > 0 && countOfServings < 5) {
+    text.text1 = 'порции';
+  } else if (countOfServings > 4) {
+    text.text1 = 'порций';
+  } else {
+    text.text1 = 'Ошибка количества порций';
+  }
+
+  if (countOfMouse === 1) {
+    text.text2 = 'мышь в подарок';
+    text.text3 = '';
+  } else if (countOfMouse > 0 && countOfMouse < 5) {
+    text.text2 = 'мыши в подарок';
+    text.text3 = '';
+  } else if (countOfMouse > 4) {
+    text.text2 = 'мышей в подарок';
+    text.text3 = 'заказчик доволен';
+  } else {
+    text.text2 = 'Ошибка количества подарочных мышей';
+    text.text3 = '';
+  }
+
+  return (
+    <div className={'textCard4'}>
+      <strong>{countOfServings}</strong>
+      {' ' + text.text1}
+      <div>
+        <strong>{countOfMouse > 1 && countOfMouse}</strong>
+        {' ' + text.text2}
+      </div>
+      <div>{text.text3}</div>
+    </div>
+  );
+}
+
 const Card = props => {
   return (
     <div id={props.id} className="card">
@@ -11,6 +51,10 @@ const Card = props => {
       <img src={Cat} alt="Cat" className="imgCat" />
       <BorderFront />
       <Underline />
+      <div className="textCard1">Сказочное заморское яство</div>
+      <div className="textCard2">Нямушка</div>
+      <div className="textCard3">{props.taste}</div>
+      <TextCard4 countOfServings={props.countOfServings} countOfMouse={props.countOfMouse} />
       <div className="bottomTextCard">
         Чего сидишь? Порадуй котэ, {/* eslint-disable-next-line */}
         <a href="#" className="bottomTextUrlCard">
